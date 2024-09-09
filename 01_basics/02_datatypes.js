bbede9c0494058647eb13434d111da4ecdafe0f6
6499907 <marketmagnetsolution@gmail.com>
2024-09-09 07:14:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,63 @@
+"use strict" //treat all JS code as a newer version
+// alert(3+3) // we are using node not browser coonsole that it run
+
+console.log(3   // code readablity should be high
+    +
+    3);
+    console.log("Abbasi");
+    // Datatypes (premitive)
+     // number >= 23 45 67
+     console.log(3+3);
+     
+     // string >=  "naveed"
+     console.log("Naveed");
+     
+     // boolean >= treu/false
+       let a = 3;
+       let b = 6;
+       console.log("a =", a ,"b =", b);
+       console.log("a>b && b>a =", a>b && b>a);
+
+     // null   >=   null (stand alone)
+     let c = null;
+     console.log(c);
+
+     // undefined
+     let companyValue;
+     console.log(companyValue);
+
+     // Bigint
+     let x = 23454323454334
+     console.log(BigInt (x));
+
+     //symbols are  (unique)
+
+
+
+    //  Datatype (non premitive)
+    //  object
+    let fullName = "Naveed Ahmad";
+    let education = "BS SE";
+    let rollNumber = 234234;
+    const institureName = "IUB"
+    rollNumber = 3344322;
+    fullName = "Abbasi Khan"
+   
+    console.log([fullName,education,rollNumber, institureName]);
+     // how to find  a datatype of the variables
+    console.log(typeof null);
+    console.log(typeof undefined);
+    console.log(typeof education);
+    console.log(typeof rollNumber);
+    
+    
+
+
+     
+
+     
+     
+
+    
+
+
