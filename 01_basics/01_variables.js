5409a2bfa3b0a7217da336c6e3a8c11ba7a8d793
6499907 <marketmagnetsolution@gmail.com>
2024-09-09 07:14:10 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+//keyowrds:: let const var
+const userId = 1234;
+let fullName = "khan sahab";
+var cityName = "Lahore";
+houseNomber = "e23g3"
+let unitState;
+// userId = 4321; // not allows to change the const value
+houseNomber = "432fre";
+cityName = "multan"
+ fullName = "hello name"
+ /* this is a multiple  line comment
+ console.log(houseNomber);
+ console.log(cityName);
+ console.log(fullName);
+ */
+ 
+ console.table([houseNomber, cityName, fullName, unitState])  // short hand property to print all of these variables at same time
+
+ 
+ 
+
